d12d2ba6428d48f39c3103ec12079e88dfc96842
saranya <saranyanambi911@gmail.com>
2024-09-29 11:50:53 +0530

    stopwatch created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-import StopWatch from './Components/Stopwatch';
+import Timer from './Components/Stopwatch';
 
 function App() {
   return (
     <div className="App">
-      <StopWatch/>
+      <Timer/>
     </div>
   );
 }
--- src/Components/Stopwatch.jsx
+++ src/Components/Stopwatch.jsx
@@ -1,40 +1,88 @@
-import react, { useEffect, useState } from 'react';
+// import react, { useEffect, useState } from 'react';
 
-const StopWatch=()=>{
-    const[timerOn,setTimeron]=useState(false)
-    const[time,setTime]=useState(0)
-    const fetchtime=(time)=>{
-        let min=Math.floor(time/60);
-        let sec=time%60;
-        return `${min}:${sec<10?"0":""}${sec}`;
+// const StopWatch=()=>{
+//     const[timerOn,setTimeron]=useState(false)
+//     const[time,setTime]=useState(0)
+//     const fetchtime=(time)=>{
+//         let min=Math.floor(time/60);
+//         let sec=time%60;
+//         return `${min}:${sec<10?"0":""}${sec}`;
+//     }
+
+//     const handlereset=()=>{
+//         setTime(0);
+//         setTimeron();
+//     }
+
+//     useEffect(()=>{
+//         let timerId;
+//         if (timerOn) {
+//           timerId = setInterval(() => {
+//             setTime((prev) => prev + 1);
+//           }, 1000);
+//         } else {
+//           clearInterval(timerId);
+//         }
+//         return(()=>{
+//              clearInterval(timerId);
+//         })
+//     },[timerOn])
+//     return(
+//         <>
+//         <h1>StopWatch</h1>
+//         <p>Time: {fetchtime(time)}</p>
+//         <button onClick={()=>setTimeron(!timerOn)}>{timerOn ?"Stop":"Start"}</button>
+//         <button onClick={()=>handlereset()}>Reset</button>
+//         </>
+//     )
+// }
+
+// export default StopWatch
+
+
+import { useState, useEffect } from "react";
+import React from "react";
+
+const Timer = () => {
+  const [timer, setTimer] = useState(0);
+  const [flag, setFlag] = useState(false);
+
+  const handleStart = () => {
+    setFlag((prev) => !prev);
+  };
+
+  const formatTime = (seconds) => {
+    const minutes = Math.floor(seconds / 60);
+    const remainingSeconds = seconds % 60;
+    return `${minutes}:${remainingSeconds < 10 ? "0" : ""}${remainingSeconds}`;
+  };
+
+  const handleReset = () => {
+    setTimer(0);
+    setFlag(false);
+  };
+  useEffect(() => {
+    let timerId;
+    if (flag) {
+      timerId = setInterval(() => {
+        setTimer((prev) => prev + 1);
+      }, 1000);
+    } else {
+      clearInterval(timerId);
     }
+    return () => {
+      clearInterval(timerId);
+    };
+  }, [flag]);
 
-    const handlereset=()=>{
-        setTime(0);
-        setTimeron(false);
-    }
+  return (
+    <div>
+      <h1>Stopwatch</h1>
+      <div style={{ paddingBottom: "15px" }}>Time: {formatTime(timer)}</div>
+      <button onClick={handleStart}>{flag ? "Stop" : "Start"}</button>
+      <button onClick={handleReset}>Reset</button>
+    </div>
+  );
+};
 
-    useEffect(()=>{
-        let timerId;
-        if (timerOn) {
-          timerId = setInterval(() => {
-            setTime((prev) => prev + 1);
-          }, 1000);
-        } else {
-          clearInterval(timerId);
-        }
-        return(()=>{
-             clearInterval(timerId);
-        })
-    },[timerOn])
-    return(
-        <>
-        <h1>StopWatch</h1>
-        <p>Time: {fetchtime(time)}</p>
-        <button onClick={()=>setTimeron(!timerOn)}>{timerOn ?"Stop":"Start"}</button>
-        <button onClick={handlereset()}>Reset</button>
-        </>
-    )
-}
-
-export default StopWatch
+export default Timer;
